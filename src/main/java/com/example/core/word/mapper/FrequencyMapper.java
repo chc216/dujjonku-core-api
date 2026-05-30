21c8fb3be9a8962a2a2255f7a6ea780c75af576d
chc216 <mine3386@naver.com>
2026-05-30 21:55:12 +0900

    chore: 변경한 도메인에 맞게 매퍼 클래스는 테이블 별로 관리하도록 변경

---
+++ src/main/java/com/example/core/word/mapper/FrequencyMapper.java
@@ -0,0 +1,7 @@
+package com.example.core.word.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FrequencyMapper {
+}
---
+++ src/main/java/com/example/core/word/mapper/WordMapper.java
@@ -0,0 +1,10 @@
+package com.example.core.word.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+//리포트에서 필요한 기능 => id를 받아서 조회하기 -> id를 넘겨서 id에 대한 정보를 가져온다. 2. id를 넘겨서 id관련 빈도수 데이터를 모두 가져온다.(84일치를 가져온다.)
+@Mapper
+public interface WordMapper {
+
+}
