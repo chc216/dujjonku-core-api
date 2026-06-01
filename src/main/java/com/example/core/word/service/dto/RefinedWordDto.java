700c07b21a8ea38e743b902f846c8fdb86bd8c48
chc216 <mine3386@naver.com>
2026-06-01 14:08:28 +0900

    feat: ai 기능을 위한 dto추가

--- src/main/java/com/example/core/word/service/dto/RefinedWordDto.java
+++ src/main/java/com/example/core/word/service/dto/RefinedWordDto.java
@@ -1,18 +1,23 @@
-package com.example.core.word.crawlingdto;
+package com.example.core.word.service.dto;
 
-import com.example.core.word.domain.Word;
+import com.example.core.word.infra.mapper.dto.WordInsertDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
-public class WordSaveRequest {
+public class RefinedWordDto {
     private String name;
     private String meaning;
     private String example;
+    @Setter
     private Integer frequency;
 
     public WordInsertDto toInsertDto(){
         return new WordInsertDto(name, meaning, example);
     }
+
 }
