a2fad6d2659c6c87ae722e4deff6a4d7b63fbaa1
chc216 <mine3386@naver.com>
2026-06-04 14:25:33 +0900

    refactor: WordProcessFacade 트랜잭션 단위 수정

--- src/main/java/com/example/core/word/service/WordProcessFacade.java
+++ src/main/java/com/example/core/word/service/WordProcessFacade.java
@@ -11,14 +11,20 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-@Transactional
 public class WordProcessFacade {
     private final WordStorageService wordStorageService;
     private final AiRefiner aiRefiner;
     private final RankingService rankingService;
 
+    //하나의 트랜잭션으로 묶어야함 -> 놉 ai기능까지 트랜잭션으로 묶어 버리면 ai통신이 끝날 때 까지 디비 커넥션을 잡고 있음 나눠야함
+    //하나의 서비스로 처리하기에는 1. ai관련 서비스 2. 단어 저장 서비스 3. 랭킹 관련 도메인을 모두 하나의 서비스에서 처리해야했음
     public void process(List<RawDataDto> rawDataList) {
         List<RefinedWordDto> refinedWordDtoList = aiRefiner.refine(rawDataList);
+
+    }
+
+    @Transactional
+    public void saveAndUpdateRanking(List<RefinedWordDto> refinedWordDtoList) {
         wordStorageService.saveWordList(refinedWordDtoList);
         rankingService.updateRanking();
     }
