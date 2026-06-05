239fb24725a33afbbecf4a9b5a40699e1016d373
jkpark <pjk163344@gmail.com>
2026-06-05 15:46:49 +0900

    feat: jwt/WebConfig.java에 CORS 설정 추가

--- src/main/java/com/example/core/jwt/WebConfig.java
+++ src/main/java/com/example/core/jwt/WebConfig.java
@@ -1,10 +1,14 @@
 package com.example.core.jwt;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor)
                 .excludePathPatterns("/admin/login")
                 .addPathPatterns("/admin/**");
+    }
+
+    @Override
+    public  void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//모든 API 주소에 대하여
+                .allowedOrigins("http://localhost:5173") //프론트엔드 주소 허용
+                .allowedMethods("GET",  "POST", "PUT", "DELETE", "PATCH",  "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 쿠키나 인증 헤더를 포함할 수 있도록 허용
+                .maxAge(3600); // Preflight 요청 캐싱 시간(1시간)
     }
 }
